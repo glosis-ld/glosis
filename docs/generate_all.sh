4c8ee342f8fef56071b56f6b52f7d53b021bfdb3
Luís de Sousa <luis.de.sousa@protonmail.ch>
2023-07-25 08:57:43 +0200

    Script and instructions for full docs creation

---
+++ docs/generate_all.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source env/bin/activate
+
+python3 generateDoc.py glosis_cl
+python3 generateDoc.py glosis_common
+python3 generateDoc.py glosis_ext_property
+python3 generateDoc.py glosis_layer_horizon
+python3 generateDoc.py glosis_main
+python3 generateDoc.py glosis_observation
+python3 generateDoc.py glosis_procedure
+python3 generateDoc.py glosis_profile
+python3 generateDoc.py glosis_siteplot
+python3 generateDoc.py glosis_surface
+python3 generateDoc.py glosis_unit
+python3 generateDoc.py iso28258
+
+deactivate
