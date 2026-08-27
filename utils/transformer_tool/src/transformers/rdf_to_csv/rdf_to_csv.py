c3b913ef5120501a2748e7a71eccfb25e92e30ef
bjaniak <bjaniak@man.poznan.pl>
2026-08-27 16:38:03 +0200

    additional fix to the transformer tool

--- utils/transformer_tool/src/transformers/rdf_to_csv/rdf_to_csv.py
+++ utils/transformer_tool/src/transformers/rdf_to_csv/rdf_to_csv.py
@@ -20,7 +20,7 @@ class Transformer(object):
         self.filename = file
         self.graph = self._parse_into_graph()
         self.file_type = file_type
-        self.attributes = []
+        self.collections = []
         self.results = {}
         self.base_uri = base_uri_mapping.get(self.file_type)
         self.output = output_filename if output_filename else os.path.splitext(file)[0]
@@ -45,36 +45,45 @@ class Transformer(object):
             print("File not found, please double-check the path that you provided!")
             return
 
-    def _get_attr_name(self, attribute):
-        attr_name = re.findall(rf"(?<={self.base_uri}).*(?=ValueCode|PropertyCollection|Procedure)", attribute)[-1]
-        postfix = re.findall(r"ValueCode|PropertyCollection|Procedure", attribute)[-1]
-        return attr_name, postfix
+    def _get_coll_name(self, collection):
+        coll_name = re.findall(rf"(?<={self.base_uri}).*(?=ValueCollection|PropertyCollection|ProcedureCollection)", collection)[-1]
+        postfix = re.findall(r"ValueCollection|PropertyCollection|Procedure", collection)[-1]
+        postfixes = {"Procedure": "Procedure", "ValueCollection": "ValueCode", "PropertyCollection": "PropertyCode"}
+        postfix_adjusted = postfixes.get(postfix)
+        return coll_name, postfix_adjusted
 
     def _get_instance_name(self, instance):
-        return re.findall(r"(?:(?<=ValueCode\-)|(?<=PropertyCollection\-)|(?<=Procedure\-)).*(?=>)", instance)[-1]
+        return re.findall(r"(?:(?<=ValueCode\-)|(?<=PropertyCode\-)|(?<=Procedure\-)).*(?=>)", instance)[-1]
 
-    def _select_attributes(self):
-        concept_scheme = rdflib.term.URIRef('http://www.w3.org/2004/02/skos/core#ConceptScheme')
+    def _select_collections(self):
+        collection = rdflib.term.URIRef('http://www.w3.org/2004/02/skos/core#Collection')
         for s, p, o in self.graph:
-            if o == concept_scheme:
-                self.attributes.append(self._get_attr_name(attribute=s.n3()))
+            if o == collection:
+                self.collections.append(self._get_coll_name(collection=s.n3()))
 
     def _select_instances(self):
         concept = rdflib.term.URIRef('http://www.w3.org/2004/02/skos/core#Concept')
         ns_type = rdflib.term.URIRef('http://www.w3.org/1999/02/22-rdf-syntax-ns#type')
-        for attribute in self.attributes:
+        for collection in self.collections:
+            #self.logger.debug(f"{collection=}")
             current_instances = []
             for s, p, o in self.graph:
                 if o == concept and p == ns_type:
-                    if any(word in s for word in [f"{attribute[0]}ValueCode", f"{attribute[0]}PropertyCode",
-                                                  f"{attribute[0]}Procedure"]):
+                    #self.logger.debug(f"{s=}")
+                    collection_name = collection[0]
+                    camel = collection_name[0].lower() + collection_name[1:]
+                    #self.logger.debug(f"{camel=}")
+                    patterns = [
+                        f"{camel}ValueCode",
+                        f"{camel}PropertyCode",
+                        f"{camel}Procedure"
+                    ]
+                    if any(pattern in str(s) for pattern in patterns):
+                        #self.logger.debug(f"MATCH: {s.n3()}")
                         instance_value = self._get_instance_name(instance=s.n3())
                         current_instances.append(instance_value)
-            self.results.update({attribute: dict.fromkeys(current_instances, {})})
-
-    @staticmethod
-    def _is_property(phrase):
-        return True if "PropertyCode" in phrase else False
+            #self.logger.debug("-----------=========--------------")
+            self.results.update({collection: dict.fromkeys(current_instances, {})})
 
     @staticmethod
     def _map_foi(foi_val):
@@ -90,21 +99,34 @@ class Transformer(object):
             return None
 
     def _get_instance_details(self):
-        for attribute in self.results.keys():
-            concept_definition = None
+        for collection, values in self.results.items():
+            collection_definition = None
             if not self.file_type == "procedure":
-                attr_uri = self.base_uri + attribute[0] + attribute[1]
+                coll_uri = self.base_uri + collection[0] + collection[1]
                 for s, p, o in self.graph:
-                    if s == attr_uri and p == rdflib.URIRef("http://www.w3.org/2004/02/skos/core#definition"):
-                        concept_definition = o.n3().strip('"')
-            if self.results[attribute]:
-                for instance in self.results[attribute].keys():
+                    if s == coll_uri and p == rdflib.URIRef("http://www.w3.org/2004/02/skos/core#definition"):
+                        collection_definition = o.n3().strip('"')
+            if not values:
+                # no instances case
+                collection_dict = {}
+                collection_dict["collection_definition"] = collection_definition
+                self.results[collection] = collection_dict
+            else:
+                #self.logger.debug(f"{self.results[collection]=}")
+                for instance in values:
+                    #self.logger.debug(f"{instance=}")
                     property_dict = {}
-                    property_dict["concept_definition"] = concept_definition
-                    property_dict["isproperty"] = self._is_property(attribute[1])
-                    instance_phrase = f"{attribute[0]}{attribute[1]}-{instance}"
+                    property_dict["collection_definition"] = collection_definition
+                    camel = collection[0][0].lower() + collection[0][1:]
+                    instance_phrase = f"{camel}{collection[1]}-{instance}"
+                    # self.logger.debug(f"{collection[0]=}")
+                    # self.logger.debug(f"{collection[1]=}")
+                    #self.logger.debug(f"{instance_phrase=}")
                     instance_uri = self.base_uri + instance_phrase
+                    #self.logger.debug(f"{instance_uri=}")
                     for s, p, o in self.graph:
+                        # self.logger.debug(f"{s=}")
+                        # self.logger.debug(f"{instance_uri=}")
                         if s == instance_uri:
                             if p == rdflib.URIRef("http://www.w3.org/2004/02/skos/core#definition"):
                                 property_dict["definition"] = o.n3().strip('"')
@@ -120,6 +142,7 @@ class Transformer(object):
                                 property_dict["pub_chem"] = o.n3().strip('"').removesuffix('"@en')
                             elif p == rdflib.URIRef("http://www.w3.org/ns/ssn/isPropertyOf"):
                                 mapped_value = self._map_foi(o.n3())
+                                #self.logger.debug(f"{mapped_value=}")
                                 if mapped_value:
                                     property_dict.setdefault("foi", []).append(mapped_value)
                             elif "scopeNote" in p:
@@ -128,17 +151,14 @@ class Transformer(object):
                                 elif isinstance(o, rdflib.term.URIRef):
                                     property_dict["reference"] = o.n3().strip("<>")
                             elif "core#broader" in p:
-                                property_dict["parent_instance"] = self._get_instance_name(o.n3())
-                    self.results[attribute][instance] = property_dict
-            else:
-                attribute_dict = {}
-                attribute_dict["concept_definition"] = concept_definition
-                attribute_dict["isproperty"] = self._is_property(attribute[1])
-                self.results[attribute] = attribute_dict
+                                property_dict["parent_concept"] = self._get_instance_name(o.n3())
+                    # self.logger.debug(f"{property_dict=}")
+                    self.results[collection][instance] = property_dict
 
     def transform_to_csv(self):
-        self._select_attributes()
+        self._select_collections()
         self._select_instances()
+        # self.logger.debug(f"{self.results=}")
         self._get_instance_details()
         frames = []
         # commented out, used only locally for debugging purposes
@@ -150,19 +170,29 @@ class Transformer(object):
                     if isinstance(self.results[k][k2], dict):
                         instance_data = self.results[k][k2]
                         normalized_instance_data = pd.json_normalize(instance_data)
-                        normalized_instance_data["instance"] = k2
-                        normalized_instance_data["attribute"] = k[0]
+                        normalized_instance_data["concept"] = k2
+                        normalized_instance_data["collection"] = k[0]
                         frames.append(normalized_instance_data)
-                    # if there are no instances save only basic information related to attribute
+                    # if there are no instances save only basic information related to collection
                     else:
                         attribute_data = self.results[k]
                         normalized_attribute_data = pd.json_normalize(attribute_data)
-                        normalized_attribute_data["attribute"] = k[0]
+                        normalized_attribute_data["collection"] = k[0]
                         frames.append(normalized_attribute_data)
+        #self.logger.debug(frames)
         df = pd.concat(frames)
-        df = df.explode('foi')
-        df = df.reindex(columns=["foi", "attribute", "instance", "parent_instance", "notation", "label", "definition",
-                                 "reference", "citation", "isproperty", "concept_definition", "pub_chem", 
-                                 "inchi_key", "inchi"])
+        #self.logger.debug(f"{df=}")
+        if self.file_type == "properties":
+            df = df.explode('foi')
+            df = df.reindex(columns=["foi", "collection", "concept", "parent_concept", "notation", "label", "definition",
+                                     "reference", "citation", "collection_definition", "pub_chem",
+                                     "inchi_key", "inchi"])
+        else:
+            df = df.reindex(columns=["collection", "concept", "parent_concept", "notation", "label", "definition",
+                                     "reference", "citation", "collection_definition", "pub_chem",
+                                     "inchi_key", "inchi"])
         df.drop_duplicates(inplace=True)
+        df["collection"] = df["collection"].apply(
+            lambda x: x[0].lower() + x[1:] if x else x
+        )
         df.to_csv(f"{self.output}", index=False)
